02ae4f1cca17096faafa2a5ed975e979e2c7525c
Julian Joonyon Park <joonyon0807@gmail.com>
2016-05-27 10:39:03 -0400

    scroll

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-  var height = $(window).height();
-  var scrollTop = $(window).scrollTop();
+//   var height = $(window).height();
+//   var scrollTop = $(window).scrollTop();
 
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 800) {
-        $('.process_box').attr('background-color', '#FFFFFF');
-    } else if (true) {
-
-    }
-  });
+  $('.process_box').scroll(function() {
+    var pos = $(this).scrollTop();
+    if (pos > 500 && pos < 800) {
+        $('.process_box').css('color', "#000000");
+        console.log('passed');
+    } else if (pos >= 800) {
+        $('.process_box').css( "background-color", "#ffffff" );
+        console.log('passed more');
+    }});
 
 });
