0edd11f880c77d5d9e5b81296bc98b8185a7b4ad
Julian Joonyon Park <joonyon0807@gmail.com>
2016-06-07 01:39:26 -0400

    subheader

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
-//   var height = $(window).height();
-//   var scrollTop = $(window).scrollTop();
+  console.log('ready');
+  /* Code for potential scrolling process box */
+  // var height = $(window).height();
+  // var scrollTop = $(window).scrollTop();
+  // $('.process_box').scroll(function() {
+  //   var pos = $(this).scrollTop();
+  //   if (pos > 500 && pos < 800) {
+  //       $('.process_box').css('color', "#000000");
+  //       console.log('passed');
+  //   } else if (pos >= 800) {
+  //       $('.process_box').css( "background-color", "#ffffff" );
+  //       console.log('passed more');
+  //   }});
 
-console.log('ready');
 
-  $('.process_box').scroll(function() {
-    var pos = $(this).scrollTop();
-    if (pos > 500 && pos < 800) {
-        $('.process_box').css('color', "#000000");
-        console.log('passed');
-    } else if (pos >= 800) {
-        $('.process_box').css( "background-color", "#ffffff" );
-        console.log('passed more');
-    }});
+
+
 
 });
