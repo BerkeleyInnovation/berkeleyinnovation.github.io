6938118c6875cefc47afbb473fe9f5cfc1d018b9
Julian Joonyon Park <joonyon0807@gmail.com>
2016-06-07 16:39:45 -0400

    fixed subheaders

--- js/main.js
+++ js/main.js
@@ -12,27 +12,57 @@ $(document).ready(function(){
   //       console.log('passed more');
   //   }});
 
-
   // Start with no visible subheader
-  $(".subheader").fadeOut(0);
+  $("#subheader_about").fadeOut(0);
+  $("#subheader_process").fadeOut(0);
+  $("#subheader_partner").fadeOut(0);
   // var topHovered = false;
   // var subHovered = false;
 
   // Hovering makes subheader appear
-  $(".active_page").hover(function(){
-    $(".subheader").fadeIn(300);
-
-    $(".subheader").hover(function(){
-      $(".subheader").fadeIn(0);
+  $("#about").hover(function(){         // ABOUT
+    $("#subheader_about").fadeIn(300);
+    $("#subheader_about").hover(function(){
+      $("#about").addClass("hovered_a");
+      $("#about").attr("style", "color: #ffc299; border-bottom: 8px solid #ffc299");
+      $("#subheader_about").fadeIn(0);
     }, function() {
-      $(".subheader").fadeOut(0);
+      $("#about").removeClass("hovered_a");
+      $("#about").attr("style", "");
+      $("#subheader_about").fadeOut(0);
     });
-
   }, function() {
-    $(".subheader").fadeOut(0);
+    $("#subheader_about").fadeOut(0);
   });
 
+  $("#process").hover(function(){         // PROCESS
+    $("#subheader_process").fadeIn(300);
+    $("#subheader_process").hover(function(){
+      $("#process").addClass("hovered_a");
+      $("#process").attr("style", "color: #ffc299; border-bottom: 8px solid #ffc299");
+      $("#subheader_process").fadeIn(0);
+    }, function() {
+      $("#process").removeClass("hovered_a");
+      $("#process").attr("style", "");
+      $("#subheader_process").fadeOut(0);
+    });
+  }, function() {
+    $("#subheader_process").fadeOut(0);
+  });
 
-
+  $("#partner").hover(function(){         // PARTNER
+      $("#subheader_partner").fadeIn(300);
+      $("#subheader_partner").hover(function(){
+        $("#partner").addClass("hovered_a");
+        $("#partner").attr("style", "color: #ffc299; border-bottom: 8px solid #ffc299");
+        $("#subheader_partner").fadeIn(0);
+      }, function() {
+        $("#partner").removeClass("hovered_a");
+        $("#partner").attr("style", "");
+        $("#subheader_partner").fadeOut(0);
+      });
+    }, function() {
+      $("#subheader_partner").fadeOut(0);
+    });
 
 });
